9614e39d6102e0cc3889a8e7d2e1cdbcee9ca1b4
Pierre THIEBERT <pierre.1.2@live.fr>
2018-04-16 15:24:26 +0200

    advance

--- controller/indexController.php
+++ controller/indexController.php
@@ -1,20 +1,43 @@
 <?php
-    if($_GET["functionCall"] != null){
-        switch($_GET["functionCall"]){
-            case 'createUser' :
-                return createUser();
-            default:
-                return 'null';
-        }
+    if(isset($_GET["functionCall"])){
+        if($_GET["functionCall"] == "checkUser")
+            checkUser();
+        if($_GET["functionCall"] == "createUser")
+            createUser();
     }
 
     function createUser(){
-        //$pseudo, $password, $cp, $mail => POST
-        $hash = getHash($mail, $pseudo);
+        $cp = $_POST["cp"];
+        $mail = $_POST["mail"];
+        $pseudo = $_POST["pseudo"];
+        $pwd = $_POST["pwd"];
+        $hash = setHash($mail, $pseudo);
+        
+        $return = false;
+        //enregistrment en base
+        if($return)
+            setcookie("sortiepartieuser", $hash, time()+2592000);
+
         return "coucou";
     }
 
-    function getHash($mail, $pseudo){
+    
+    function checkUser(){
+        return json_encode("truc");
+        // $hashcookie = getHash();
+        // if($hashcookie != null){
+        //     //recuperation hash en bdd
+        //     if($user != null)
+        //         return true;
+        // }
+        // return false;
+    }
+           
+    function setHash($mail, $pseudo){
+        return md5(crypt($mail, $pseudo));
+    }
 
+    function getHash(){
+        return $_COOKIE["sortiepartieuser"];
     }
 ?>
